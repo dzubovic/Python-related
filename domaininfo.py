9499f5fa2ec21b9e55654ab7beb5c96ae3f497d9
Damir Zubovic <dzubovic@gmail.com>
2019-05-06 20:51:18 +0200

    Update domaininfo.py
    V 1.0

--- domaininfo.py
+++ domaininfo.py
@@ -4,17 +4,23 @@ more info on https://pypi.org/project/python-whois/
 """
 import whois
 
-host = input('Host:') 
-res = whois.whois(host)
-with open("domaininfo.txt", "w") as out_file:
-    out_string = "'"
-    out_string += (host)
-    out_string += "','" + res.registrar
-    out_string += "','" + str(res.name_servers[0])
-    out_string += "','" + str(res.name_servers[1])
-    out_file.write(out_string)
-print ("Domain: ", host)
-print ("Registrar: ", res.registrar)
-print ("Creation date: ", res.creation_date)
-print ("Expiration date: ", res.expiration_date)
-print ("Name servers: ", res.name_servers)
+while True:
+    host = input("Enter Domain name: ")
+    if host == "q":
+        print ("Script finished")
+        break
+
+    else:
+        res = whois.whois(host)
+    with open("whois-" + host + ".txt", "w") as out_file:
+        out_string = "'"
+        out_string += (host)
+        out_string += "','" + res.registrar
+        out_string += "','" + str(res.name_servers[0])
+        out_string += "','" + str(res.name_servers[1])
+        out_file.write(out_string)
+        print ("Domain: ", host)
+        print ("Registrar: ", res.registrar)
+        print ("Creation date: ", res.creation_date)
+        print ("Expiration date: ", res.expiration_date)
+        print ("Name servers: ", res.name_servers)
