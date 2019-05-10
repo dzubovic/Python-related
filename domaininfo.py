2faa47e086c87b295f356b474884e95b6498e9c8
Damir Zubovic <dzubovic@gmail.com>
2019-05-10 09:03:26 +0200

    Update domaininfo.py
    Formatting IP address and Name servers

--- domaininfo.py
+++ domaininfo.py
@@ -18,14 +18,15 @@ while True:
         out_string += (host)
         out_string += "','" + res.registrar
         out_string += "','" + str (res1[2])
-        out_string += "','" + str(res.name_servers[0])
+        out_string += "','" + str(res.name_servers[0]) + "\n"
         out_string += "','" + str(res.name_servers[1])
         out_string += "'"
         out_file.write(out_string)
         print ("Domain: ", host)
         print ("AliasList: ", res1[1])
-        print ("IPs: ", res1[2])
+        print ("IPs: {0[0]}".format(res1[2]))
         print ("Registrar: ", res.registrar)
         print ("Creation date: ", res.creation_date)
         print ("Expiration date: ", res.expiration_date)
-        print ("Name servers: ", res.name_servers[0], res.name_servers[1])
+        print ("Name servers: ", "\n", res.name_servers[0], "\n", res.name_servers[1])
+
